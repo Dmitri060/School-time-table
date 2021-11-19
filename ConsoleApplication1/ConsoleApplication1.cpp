ca35708890404b0b1415066cf5728a0b41036518
Дима Никитин <Nikitin.di201@yandex.ru>
2021-11-19 15:46:59 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+int main()
+{
+    std::vector<std::string> schoolObject;
+	{
+		std::string a;
+		while (1)
+		{
+			std::cout << "Введите предмет: ";
+			std::getline(std::cin, a);
+			if (a != "stop")
+				schoolObject.push_back(a);
+		}
+	}
+}
