40ac9371895b85469d4f33465ae5082a3e86be0d
Дима Никитин <Nikitin.di201@yandex.ru>
2021-11-21 17:36:39 +0500

    0

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,18 +1,72 @@
 ﻿#include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
+
+class Teacher
+{
+
+	std::string m_name;
+	std::vector<std::string> &m_schoolObject;
+
+public:
+	Teacher(std::string name, std::vector<std::string> &schoolObject) : m_name(name), m_schoolObject(schoolObject)
+	{
+
+	}
+	~Teacher()
+	{}
+};
+
+std::string cinSchoolObject(std::string text = "Введите предмет(stop): ")
+{
+	while (true)
+	{
+		std::cout << text;
+		std::string a;
+		std::getline(std::cin, a);
+
+		if (std::cin.fail()) 
+		{
+			std::cin.clear(); 
+			std::cin.ignore(32767, '\n');
+		}
+		else
+			return a;
+	}
+}
 
 int main()
 {
+	setlocale(LC_ALL, "Russian");
+#pragma region initSchoolObject
+
     std::vector<std::string> schoolObject;
+	while (true)
 	{
-		std::string a;
-		while (1)
+		std::string object(cinSchoolObject());
+		if (object == "stop")
+			break;
+		schoolObject.push_back(object);
+	}
+
+	std::sort(schoolObject.begin(), schoolObject.end());
+
+#pragma endregion
+
+#pragma region initTeacher
+
+	std::vector<Teacher> teacher;
+	while (true)
+	{
+		std::string name(cinSchoolObject("Введите имя учителя: "));
+		std::vector<std::string*> object;
+		while (true)
 		{
-			std::cout << "Введите предмет: ";
-			std::getline(std::cin, a);
-			if (a != "stop")
-				schoolObject.push_back(a);
+
 		}
 	}
+
+#pragma endregion
+
 }
